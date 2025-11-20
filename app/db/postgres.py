75a14b1ccf185327d83627aa76893c92ac49acd5
spencerhum04 <spencerhum04@gmail.com>
2025-11-20 00:28:34 -0500

    feat: initialize agentic rag architecture with fastapi and postgres

--- app/db/postgres.py
+++ app/db/postgres.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from langchain_postgres import PGVector
 from langchain_openai import OpenAIEmbeddings
 
+# Connect to the Docker database
 connection = "postgresql+psycopg://admin:password@localhost:5432/vector_db"
 collection_name = "company_docs"
 
